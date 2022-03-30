495706c916d8105eb1051238e4ac81481d2a21f6
John <john.doe@doe.com>
2022-03-30 22:14:56 +0200

    Test1

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React Custom
           </a>
         </header>
       </div>
